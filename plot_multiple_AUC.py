7c30e7158fbacc36b3b2254344cd7601e0377480
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-09-13 17:00:24 +0100

    Update plot_multiple_AUC.py

--- plot_multiple_AUC.py
+++ plot_multiple_AUC.py
@@ -102,3 +102,76 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Training loop and data loading will depend on your specific dataset.
+
+
+
+
+
+
+
+# First, save the ground truth and the prediction when testing the model to a dataframe 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve,auc
+import numpy as np
+path = r"E:\BMVC_rebuttel\TruePred_data_CNN.csv"
+data = pd.read_csv(path)
+path2 = r"E:\BMVC_rebuttel\TruePred_cross_OA.csv"
+data2 = pd.read_csv(path2)
+
+path3 =  r"E:\BMVC_rebuttel\TruePred_stanA_mlp.csv"
+data3 = pd.read_csv(path3)
+
+path4 =  r"E:\BMVC_rebuttel\TruePred_cross_od.csv"
+data4 = pd.read_csv(path4)
+
+path5 =  r"E:\BMVC_rebuttel\TruePred_FOAA.csv"
+data5 = pd.read_csv(path5)
+
+
+fpr1, tpr1, thresholds1 = roc_curve(data['true'], data['pred'])
+roc_auc1 = auc(fpr1, tpr1)
+
+fpr2, tpr2, thresholds2 = roc_curve(data2['true'], data2['pred'])
+roc_auc2 = auc(fpr2, tpr2)
+
+fpr3, tpr3, thresholds3 = roc_curve(data3['true'], data3['pred'])
+roc_auc3 = auc(fpr3, tpr3)
+
+
+fpr4, tpr4, thresholds4 = roc_curve(data4['true'], data4['pred'])
+roc_auc4 = auc(fpr4, tpr4)
+
+
+fpr5, tpr5, thresholds5 = roc_curve(data5['true'], data5['pred'])
+roc_auc5 = auc(fpr5, tpr5)
+
+
+fig = plt.figure(figsize=(8,8))
+
+plt.plot(fpr1, tpr1, color='green', lw=2, label=f'CNN-Only (AUC = {roc_auc1:.2f})')
+
+# Plot the second AUC curve
+plt.plot(fpr2, tpr2, color='navy', lw=2, label=f'CNN + MLP standardA (AUC = {roc_auc2:.2f})')
+
+#plt.plot(fpr3, tpr3, color='red', lw=2, label=f'CNN branch(AUC = {roc_auc3:.2f})')
+
+#plt.plot(fpr4, tpr4, color='green', lw=2, label=f'CNN branch(AUC = {roc_auc4:.2f})')
+
+plt.plot(fpr5, tpr5, color='red', lw=3, label=f'FOAA (AUC = {roc_auc5:.2f})')
+    
+plt.plot([0,1], [0,1], color='orange', linestyle='--')
+
+plt.xlim([0.0, 1.01])
+plt.ylim([0.0, 1.01])
+
+plt.xticks(np.arange(0.0, 1.1, step=0.1))
+plt.xlabel("Flase Positive Rate", fontsize=15)
+
+plt.yticks(np.arange(0.0, 1.1, step=0.1))
+plt.ylabel("True Positive Rate", fontsize=15)
+
+plt.title('Receiver Operating Characteristic (ROC) Curve', fontweight='bold', fontsize=15)
+plt.legend(prop={'size':13}, loc='lower right')
+
+plt.show()
